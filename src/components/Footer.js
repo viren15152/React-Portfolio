70d30c4eddeb2193c8346bca4f052f5babd1e90b
Viren <144032304+viren15152@users.noreply.github.com>
2024-02-24 06:09:59 +0000

    code done for my footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; {new Date().getFullYear()} Mr Viren Lala. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
