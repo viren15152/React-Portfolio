8f43d396c8193cf40d447ebc000af686738dd965
Viren <144032304+viren15152@users.noreply.github.com>
2024-02-25 16:59:58 +0000

    Added the html to my index.html and the jsx to my Navigation.js

---
+++ src/components/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function Navigation() {
+  return (
+    <nav>
+      <NavLink to="/" activeClassName="active" exact>About Me</NavLink>
+      <NavLink to="/portfolio" activeClassName="active">Portfolio</NavLink>
+      <NavLink to="/contact" activeClassName="active">Contact</NavLink>
+      <NavLink to="/resume" activeClassName="active">Resume</NavLink>
+    </nav>
+  );
+}
+
+export default Navigation;
