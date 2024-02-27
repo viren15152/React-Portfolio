2535e70063941993a96a22ff726b303e2150b56a
Viren <144032304+viren15152@users.noreply.github.com>
2024-02-27 15:14:04 +0000

    Added bsae code for my resume.js file will finish it when I've completed my resume

---
+++ src/components/Resume.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Resume() {
+  return (
+    <div>
+      <h2>Resume</h2>
+      <p>Work Experience:</p>
+      <ul>
+        <li>Job 1</li>
+        <li>Job 2</li>
+        {/* I will add more items when needed when I create my Resume */}
+      </ul>
+      {/* I will add more resume sections like education, skills, etc when I have completed my Resume. */}
+    </div>
+  );
+}
+
+export default Resume;
