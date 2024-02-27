068b7b6f9c9c9f066c28dbb2d1cc9643210293f4
Viren <144032304+viren15152@users.noreply.github.com>
2024-02-27 18:18:50 +0000

    Updated paths to images and changed Port in vite.config.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,11 +3,11 @@ import Project from './Project';
 
 function Portfolio() {
   const projects = [
-    { id: 1, title: 'Weather Dashboard', image: '../images/weather-dashboard.png', deployedLink: 'https://viren15152.github.io/06-Weather-Dashboard-Server-Side-APIs-Viren/', githubLink: 'https://github.com/viren15152/06-Weather-Dashboard-Server-Side-APIs-Viren' },
-    { id: 2, title: 'CMS Tech Blog', image: '../images/cms-tech-blog.png', deployedLink: 'https://secure-journey-14586-f3f5cbbb08df.herokuapp.com/login', githubLink: 'https://github.com/viren15152/Model-View-Controller-CMS-Tech-Blog' },
-    { id: 3, title: 'Advanced-CSS-Portfolio', image: '../images/advanced-css-portfolio.png', deployedLink: 'https://viren15152.github.io/Advanced-CSS-Portfolio/', githubLink: 'https://github.com/viren15152/Advanced-CSS-Portfolio' },
-    { id: 4, title: 'Web API Coding Quiz', image: '../images/web-api-coding-quiz.png', deployedLink: 'https://viren15152.github.io/Web-APIs-Coding-Quiz/', githubLink: 'https://github.com/viren15152/Web-APIs-Coding-Quiz' },
-    { id: 5, title: 'PWA Text Editor', image: '../images/pwa-text-editor.png', deployedLink: 'https://progressive-web-applications-pwa-text-w3iw.onrender.com/', githubLink: 'https://github.com/viren15152/Progressive-Web-Applications-PWA-Text-Editor' },
+    { id: 1, title: 'Weather Dashboard', image: '../images/Weather App.png', deployedLink: 'https://viren15152.github.io/06-Weather-Dashboard-Server-Side-APIs-Viren/', githubLink: 'https://github.com/viren15152/06-Weather-Dashboard-Server-Side-APIs-Viren' },
+    { id: 2, title: 'CMS Tech Blog', image: '../images/CMS Tech Blog.png', deployedLink: 'https://secure-journey-14586-f3f5cbbb08df.herokuapp.com/login', githubLink: 'https://github.com/viren15152/Model-View-Controller-CMS-Tech-Blog' },
+    { id: 3, title: 'Advanced-CSS-Portfolio', image: '../images/Advanced CSS Portfolio.png', deployedLink: 'https://viren15152.github.io/Advanced-CSS-Portfolio/', githubLink: 'https://github.com/viren15152/Advanced-CSS-Portfolio' },
+    { id: 4, title: 'Web API Coding Quiz', image: '../images/Coding Quiz.png', deployedLink: 'https://viren15152.github.io/Web-APIs-Coding-Quiz/', githubLink: 'https://github.com/viren15152/Web-APIs-Coding-Quiz' },
+    { id: 5, title: 'PWA Text Editor', image: '../images/JATE App.png', deployedLink: 'https://progressive-web-applications-pwa-text-w3iw.onrender.com/', githubLink: 'https://github.com/viren15152/Progressive-Web-Applications-PWA-Text-Editor' },
   ];
 
   return (
