67e213ad958ed39f8694cf01e1e41e8ebb62987c
Viren <144032304+viren15152@users.noreply.github.com>
2024-02-29 18:50:21 +0000

    Added comments to my index.js file

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'; // Importing React library
+import ReactDOM from 'react-dom'; // Importing ReactDOM library
+import App from './App'; // Importing the main component of your application
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
+ReactDOM.render( // Rendering the main component to the root element
+  <React.StrictMode> {/* Wrapping your component with StrictMode for additional checks */}
+    <App /> {/* Rendering your main component */}
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root') // Mounting point of your React application
 );
+
